7641872527391190cc76da8433b2d4e9a189c566
Loony213 <150311939+Loony213@users.noreply.github.com>
2025-07-02 15:36:07 -0500

    new option

--- delete_account/models/user.rb
+++ delete_account/models/user.rb
@@ -1,18 +1,22 @@
 require_relative '../db/db'
 
 class User
-  # Métodos para interactuar con la base de datos
-
   # Método para eliminar un usuario por su correo electrónico
   def self.delete_user(email)
     # Consulta SQL para eliminar al usuario
-    result = DB_CLIENT.execute("DELETE FROM users WHERE email = '#{email}'")
+    begin
+      result = DB_CLIENT.execute("DELETE FROM users WHERE email = '#{email}'")
 
-    # Verifica si el número de filas afectadas es mayor que 0
-    if result
-      return true  # El usuario fue eliminado
-    else
-      return false # No se encontró el usuario
+      # Verifica si la operación afectó alguna fila
+      if result && result.count > 0
+        return true  # El usuario fue eliminado
+      else
+        return false # No se encontró el usuario
+      end
+    rescue StandardError => e
+      puts "Error al eliminar el usuario: #{e.message}"
+      return false
     end
   end
 end
+
